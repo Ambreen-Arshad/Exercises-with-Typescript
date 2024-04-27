587feaf6c04c2fef4019ab3ca839bb06c75053ec
Ambreen-Arshad <161050342+Ambreen-Arshad@users.noreply.github.com>
2024-04-27 20:49:58 +0500

    4th commit

---
+++ famous_quote/index.js
@@ -0,0 +1,6 @@
+// Date 27-4-2024
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.\n
+// Your output should look something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
+export {};
